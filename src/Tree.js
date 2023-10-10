99ed882e4d88ebc4cb27b44a5cbdbcb311b713f9
Levi Keck <keck.140@buckeyemail.osu.edu>
2023-10-09 20:26:25 -0400

    Added tree class

---
+++ src/Tree.js
@@ -0,0 +1,16 @@
+import Phaser from "phaser";
+import Physics from "phaser";
+import Entity from "entity";
+import treeImg from "../assets/tree.png";
+
+/** 
+ * 
+ * Class representing a tree object
+*/
+export default class Tree extends Entity{
+    constructor(x, y, texture, w, h, depth = 1) {
+        super(x, y, texture);
+        this.texture = treeImg;
+    }
+
+}
