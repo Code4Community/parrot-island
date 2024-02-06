33ac79563c4d68e30fce69cba8ab93b4819a1c5b
Your Name <squirrellord19@gmail.com>
2024-02-05 20:05:10 -0500

    remove old functions; import entities from JSON

--- src/level.js
+++ src/level.js
@@ -1,15 +1,6 @@
 // @ts-check
 
-
-// /**
-//  * @param {string} filePath - the (website) path of the file to load.
-//  * @return {Promise<LevelData>} - the level data.
-//  */
-// export const readLevelData = async (filePath) => {
-//     /** @type LevelData */
-//     const levelData = await fetch(filePath).then((response) => response.json());
-//     return levelData;
-// };
+import Entity from "./entity";
 
 /**
  * @param {LevelData} levelData - the level data.
@@ -46,84 +37,24 @@ export const GenerateSceneFromLevelData = (levelData, scene, tileSize) =>{
     }
     
     for(var i = 0; i < levelData.entities.length; i++){
-        let entity = levelData.entities[i];
-        //scene.entities.push(entity);
+        let data = levelData.entities[i];
+        let entity = new Entity(data.x, data.y, data.texture, data.size);
+        scene.entities.push(entity);
     }
 }
 
-// /**
-//  * @param {string} name
-//  * @param {number} width
-//  * @param {number} height
-//  * @return {LevelData}
-//  */
-// export const generateRandomLevelData = (name, width, height) => {
-
-//     /**
-//      * @type {Tile[][]}
-//      */
-//     const tiles = [];
-
-//     /**
-//      * @type {Entity[]}
-//      */
-//     const entities = [];
-
-//     for (let y = 0; y < height; y++) {
-//         /**
-//          * @type {Tile[]}
-//          */
-//         let tileRow = [];
-//         /**
-//          * @type {Entity[]}
-//          */
-//         let entityRow = [];
-//         for (let x = 0; x < width; x++) {
-//             /**
-//              * @type Tile
-//              */
-//             let tile;
-
-//             if (Math.random() < 0.5) {
-//                 tile = "grass";
-//             } else {
-//                 tile = "stone";
-//             }
-
-//             if (Math.random() < 0.1) {
-//                 entityRow.push("tree");
-//             } else {
-//                 entityRow.push(null);
-//             }
-
-//             tileRow.push(tile);
-//         }
-//         tiles.push(tileRow);
-//     }
-
-//     /**
-//      * @type LevelData
-//      */
-//     const levelData = {
-//         name,
-//         width,
-//         height,
-//         tiles,
-//         entities,
-//     };
-
-//     return levelData;
-// };
-
 /**
- * @typedef { 'grass' | 'stone' } Tile
- * @typedef { 'tree' | null } Entity
  *
  * @typedef {{
  *      name: string,
  *      width: number,
  *      height: number, 
  *      tiles: number[][],    // indexed by [y][x]
- *      entities: Entity[], //
+ *      entities: {
+ *                  x : number,
+ *                  y : number,
+ *                  texture: string,
+ *                  size : number
+ *                }[], //
  * }} LevelData
  */
