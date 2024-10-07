4b45545ff065231d1f1f5fc4a87dbf6e43947b06
Kent Gifford <gifford.197@buckeyemail.osu.edu>
2024-10-07 19:34:18 -0400

    Revert "added cannons but need to add intervals"
    This reverts commit 5e76252914febca14bd7d8548131ba8716978e43.

--- src/levels.js
+++ src/levels.js
@@ -1,15 +1,9 @@
 import level1JSON from './assets/levels/level1.json';
 import level2JSON from './assets/levels/level2.json';
 import level3JSON from './assets/levels/level3.json';
-import level4JSON from './assets/levels/level4.json';
-import level5JSON from './assets/levels/level5.json';
-import level6JSON from './assets/levels/level6.json';
 
 export const levels = {
     'level1': level1JSON,
     'level2': level2JSON,
     'level3': level3JSON,
-    'level4': level4JSON,
-    'level5': level5JSON,
-    'level6': level6JSON,
 }
