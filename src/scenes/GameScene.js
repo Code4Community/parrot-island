ed1875da5c35cb0261d3fa918ab23380870dbb97
Kent Gifford <gifford.197@buckeyemail.osu.edu>
2024-11-18 20:09:32 -0500

    Added new win screen

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -12,7 +12,7 @@ import sandImg from "../assets/sandNew.png";
 import cannonballImg from "../assets/cannonball.png"
 import blankImg from "../assets/blank.png"
 import gameOverImg from "../assets/gameOver.png"
-import gameWinImg from "../assets/gameWin.png"
+import gameWinImg from "../assets/GameWinNew.png"
 import unloadedCannonImg from "../assets/unloadedCannon.png"
 import loadedCannonImg from "../assets/loadedCannon.png"
 import leftSwitchImg from "../assets/leftSwitch.png"
