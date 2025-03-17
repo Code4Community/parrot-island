864839871b619520cfd710a7b4657c6a7bb0f612
Kent Gifford <gifford.197@buckeyemail.osu.edu>
2025-03-17 19:41:46 -0400

    added level 7 and that entails

--- src/levels.js
+++ src/levels.js
@@ -4,6 +4,7 @@ import level3JSON from './assets/levels/level3.json';
 import level4JSON from './assets/levels/level4.json';
 import level5JSON from './assets/levels/level5.json';
 import level6JSON from './assets/levels/level6.json';
+import level7JSON from './assets/levels/level7.json';
 
 export const levels = {
     'level1': level1JSON,
@@ -12,4 +13,5 @@ export const levels = {
     'level4': level4JSON,
     'level5': level5JSON,
     'level6': level6JSON,
+    'level7': level7JSON,
 }
